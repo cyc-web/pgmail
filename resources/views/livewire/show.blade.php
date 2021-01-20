c840f3ef30b107a3aa8ab13ae071f8e13765a4e7
royalcode <royalcode1005@gmail.com>
2021-01-20 12:25:00 +0100

    updated version

--- resources/views/livewire/show.blade.php
+++ resources/views/livewire/show.blade.php
@@ -17,12 +17,12 @@
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <!--div class="image">
-          <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div-->
+     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="/{{'storage/profile/'.Auth::user()->passport}}" class="brand-image img-circle elevation-3" alt="User Image">
+        </div>
         <div class="info">
-          <a href="#" class="d-block"><i class="fa fa-circle text-success"></i> {{Auth::user()->name}}</a>
+          <a href="/profile" class="d-block"><i class="fa fa-circle text-success"></i> {{Auth::user()->name}}</a>
         </div>
       </div>
 
